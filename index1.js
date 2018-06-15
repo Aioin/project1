8310241261a741c9d4bd18783a398fbf9a643344
Aioin <3041428231@qq.com>
2018-06-15 08:41:39 +0800

    在分之2修改了index.js

---
+++ index1.js
@@ -0,0 +1,15 @@
+var oLi = document.getElementsByTagName('li');
+var oBox = document.getElementsByClassName('box')
+for(var i=0;i<oLi.length;i++){
+    oLi[i].index = i
+    oli[i].onclick=function(){
+      for(var j=0;j<oLi.length;j++){
+            oLi[j].className="";
+      }
+      oLi[this.index].className="active";
+      for(var i=0;i<oBox.length;i++){
+        oBox[i].style.display="none";
+      }
+      oBox[this.index].style.display="block";
+    }
+}
